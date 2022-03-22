0abd80a29af674f159fc28ea6e989b0d7dd0b2ed
Harry <hareshwarp649>
2022-03-22 09:51:08 +0530

    Create Method to Analyse mood message

---
+++ AnalyseMoodfirst.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mtest1
+{
+    public class AnalyseMoodfirst
+    {
+      
+       public string message1;
+
+            public AnalyseMoodfirst(string message1)
+            {
+                this.message1 = message1;
+            }
+
+            public string AnalysingMood()
+            {
+                 if (this.message1.ToLower().Contains("sad"))
+                    {
+                        return "SAD";
+                    }
+                    else
+                    {
+                        return "HAPPY";
+                    }
+
+                
+           }
+        
+    }
+}
