bdedea9782151c95e76b065012ac3a9bea5a4192
Harry <hareshwarp649>
2022-03-22 13:12:35 +0530

    Custom Exception MoodAnalysisException added with TC 3.1 & 3.2

---
+++ CustomExceptionSec.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mtest3
+{
+    public class CustomExceptionSec:Exception
+    {
+        public ExceptionTypes type;
+
+        //Enum to differentiate mood analysis errors
+        public enum ExceptionTypes
+        {
+            NULL_MOOD_EXCEPTION,
+            EMPTY_MOOD_EXCEPTION
+        }
+
+        //Constructor to initialize ExceptionTypes
+        public CustomExceptionSec(ExceptionTypes type, string message) : base(message)
+        {
+            this.type = type;
+        }
+
+    }
+}
