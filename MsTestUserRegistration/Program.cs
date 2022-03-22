62b4eddc7d72ddbae27340c5acf318a30f436091
Harry <hareshwarp649>
2022-03-22 22:01:38 +0530

    User Registration using MsTest

---
+++ MsTestUserRegistration/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsTestUserRegistration
+{
+    public class Program
+    {
+        public static void UserRegisration()
+        {
+
+
+            bool Continue = true;
+            while (Continue)
+            {
+                Console.WriteLine("Choose Option to Validate with their specified Pattern");
+                Console.WriteLine("1 = First Name\n2 = Last Name\n3 = Mobile Number\n4 = Password\n0 = Exit");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        Continue = false;
+                        break;
+                    case 1:
+                        Console.WriteLine("Enter First Name");
+                        string firstName = Console.ReadLine();
+                        Console.WriteLine(UserRegistration.FirstName(firstName));
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter Last Name");
+                        string lastName = Console.ReadLine();
+                        Console.WriteLine(UserRegistration.FirstName(lastName));
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter Mobile Number");
+                        string mobileNumber = Console.ReadLine();
+                        Console.WriteLine(UserRegistration.MobileNumber(mobileNumber));
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter PassWord");
+                        string password = Console.ReadLine();
+                        Console.WriteLine(UserRegistration.Password(password));
+                        break;
+                    default:
+                        Console.WriteLine("Enter Correct Option!");
+                        break;
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program");
+            UserRegisration();
+            Console.ReadLine();
+
+        }
+    }
+    
+}
---
+++ MsTestUserRegistration/UserRegistration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MsTestUserRegistration
+{
+    public class UserRegistration
+    {
+        public static string firstNamePattern = "^[A-Z]{1}[a-z]{2}";
+        public static string lastNamePattern = "^[A-Z]{1}[a-z]{2}";
+        public static string mobilePattern = "^([9][1])[ ]*[6-9]{1}[0-9]{9}$";
+        public static string passwordPatternR3 = "^(?=.*?[0-9])(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[@#$&*.!?]).{8,}";
+
+        public static string Pattern(string userInput, string pattern)
+        {
+            if (Regex.IsMatch(userInput, pattern))
+                return "true";
+            else
+                return "false";
+        }
+        
+        public static string FirstName(string name)
+        {
+            var firstNameMatch = Pattern(name, firstNamePattern);
+            return firstNameMatch;
+        }
+        
+        public static string LastName(string name)
+        {
+            var lastNameMatch = Pattern(name, lastNamePattern);
+            return lastNameMatch;
+        }
+        
+        public static string MobileNumber(string number)
+        {
+            var numberMatch = Pattern(number, mobilePattern);
+            return numberMatch;
+        }
+        
+        public static string Password(string pass)
+        {
+            var passMatch = Pattern(pass, passwordPatternR3);
+            return passMatch;
+        }
+    }
+}
