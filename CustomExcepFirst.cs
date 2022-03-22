7fda4360a4a49e11aff7ae6c3528282b9edd4672
Harry <hareshwarp649>
2022-03-22 13:07:33 +0530

    Custom Exception MoodAnalysisException added with TC 3.1 & 3.2

---
+++ CustomExcepFirst.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mtest3
+{
+    public class CustomExcepFirst
+    {
+
+        public string message;
+
+        //Constructor for initializing the message
+        public CustomExcepFirst(string message)
+        {
+            this.message = message;
+        }
+
+        //Method to analyse mood form a given message
+        public string AnalyseMood()
+        {
+            //Custom Exception Handling
+            try
+            {
+                if (this.message.Equals(null))
+                {
+                    throw new CustomExceptionSec(CustomExceptionSec.ExceptionTypes.NULL_MOOD_EXCEPTION, "Message should not be null");
+                }
+                else if (this.message.Equals(string.Empty))
+                {
+                    throw new CustomExceptionSec(CustomExceptionSec.ExceptionTypes.EMPTY_MOOD_EXCEPTION, "Message should not be empty");
+                }
+                else if (this.message.ToLower().Contains("sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (CustomExceptionSec)
+            {
+                return "HAPPY";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "HAPPY";
+            }
+        }
+    }
+}
