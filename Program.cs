3adb46937d8b9e08f89a859fbf51fe1e46f81a92
Harry <hareshwarp649>
2022-03-22 09:29:23 +0530

    First MsTest Program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mtest1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" What is your Mood!");
+            AnalyseMoodfirst message1= new AnalyseMoodfirst("i am ");
+           Console.WriteLine( message1.AnalysingMood());
+            Console.ReadLine();
+        }
+    }
+}
