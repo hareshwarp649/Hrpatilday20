dbcb172ff70b059b24c906b6de977d648eaabef6
Harry <hareshwarp649>
2022-03-22 12:24:40 +0530

    Test case pass Handle null Scenario using try catch

---
+++ HandleExcepFirst.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTest1a
+{
+    public class HandleExcepFirst
+    {
+      
+            public string message;
+
+            //Constructor for initializing the message
+            public HandleExcepFirst(string message)
+            {
+                this.message = message;
+            }
+
+            //Method to analyse mood form a given message
+            public string AnalyseMood()
+            {
+                try
+                {
+                    if (this.message.ToLower().Contains("sad"))
+                    {
+                        return "SAD";
+                    }
+                    else
+                    {
+                        return "HAPPY";
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    return "HAPPY";
+                }
+            }
+        
+    }
+}
