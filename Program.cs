7c085ebf6b776918cf1bc67b326bce8597ca2572
Harry <hareshwarp649>
2022-03-22 12:15:23 +0530

    Handle Exception _Invalid Mood

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTest1a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Anayzer Program");
+
+            
+            HandleExcepFirst moodAnalyzer = new HandleExcepFirst("I am in Happy Mood");
+            Console.WriteLine("Mood is: " + moodAnalyzer.AnalyseMood());
+            Console.ReadLine();
+        }
+    }
+}
